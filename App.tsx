28f3bb0b356d56246ddfb1334470a6cae7e0eff9
LeeSeungHun <dos07008@naver.com>
2023-07-19 00:44:11 +0900

    refactor: 라우팅 방식 변경 stack 제거

--- App.tsx
+++ App.tsx
@@ -1,49 +1,54 @@
-import React from "react";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import {
-  createStackNavigator,
-  TransitionPresets,
-} from "@react-navigation/stack";
-
-import WebviewContainer from "./WebviewContainer";
-
-const Stack = createStackNavigator();
+import React, { useEffect, useRef } from "react";
+import { BackHandler, Platform } from "react-native";
+import { WebView, WebViewMessageEvent } from "react-native-webview";
 
 const App = () => {
+  const webViewRef = useRef(null);
+  const historyStack = useRef(0);
+
+  const onAndroidBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      if (historyStack.current === 0) {
+        return false;
+      }
+      historyStack.current =
+        historyStack.current === 0 ? 0 : historyStack.current - 1;
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+      return () => {
+        BackHandler.removeEventListener(
+          "hardwareBackPress",
+          onAndroidBackPress
+        );
+      };
+    }
+  }, []);
+
+  const requestOnMessage = async (e: WebViewMessageEvent): Promise<void> => {
+    const nativeEvent = JSON.parse(e.nativeEvent.data);
+    if (nativeEvent?.type === "ROUTER_EVENT") {
+      const path: string = nativeEvent.data;
+      if (path === "back") {
+        onAndroidBackPress();
+      } else {
+        historyStack.current += 1;
+      }
+    }
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Details"
-        screenOptions={{
-          ...TransitionPresets.SlideFromRightIOS,
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen
-          options={{
-            transitionSpec: {
-              open: {
-                animation: "spring",
-                config: {
-                  stiffness: 2000,
-                  damping: 1000,
-                },
-              },
-              close: {
-                animation: "spring",
-                config: {
-                  stiffness: 1000,
-                  damping: 500,
-                },
-              },
-            },
-          }}
-          name="Details"
-          component={WebviewContainer}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <WebView
+      source={{ uri: "http://192.168.0.101:3000" }}
+      ref={webViewRef}
+      onMessage={requestOnMessage}
+    />
   );
 };
 export default App;
